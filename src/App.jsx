36897607bddde7a07559f53ae932d4d684fc4fe3
Pranavt-2004 <pranavtambvekar0431@gmail.com>
2026-04-18 12:55:57 +0530

    updated internship app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,45 @@ function App() {
               <Route path="/jobs" element={<WFHJobsPage />} />
               <Route path="/jobs/detail/:id" element={<JobDetailPage />} />
               <Route path="/jobs/apply/:id" element={<ApplyForm />} />
+
+              {/* 1. Main Landing Pages */}
+            <Route path="/" element={<InternshipPage />} />
+            <Route path="/internships" element={<InternshipPage />} />
+            
+            {/* 2. CATEGORY ROUTES (Profile Section) */}
+            <Route 
+              path="/internships/computer-science-internship" 
+              element={<ComputerScienceInternship />} 
+            />
+            <Route 
+              path="/internships/marketing-internship" 
+              element={<MarketingInternship />} 
+            />
+            {/* Added Finance Category Route */}
+            <Route 
+              path="/internships/finance-internship" 
+              element={<FinanceInternship />} 
+            />
+
+            {/* 3. LOCATION ROUTES */}
+            <Route path="/internships/work-from-home" element={<WorkFromHome />} />
+            <Route path="/internships/internship-in-bangalore" element={<BangaloreInternships />} />
+            <Route path="/internships/internship-in-delhi" element={<DelhiInternships />} />
+            <Route path="/internships/internship-in-hyderabad" element={<HyderabadInternships />} />
+            <Route path="/internships/internship-in-:cityName" element={<CityInternshipPage />} />
+            
+            {/* 4. DYNAMIC DETAIL VIEW */}
+            {/* Handles all IDs: 500s (CS), 600s (Marketing), 700s (Finance) */}
+            <Route path="/internship/:id" element={<InternshipDetail />} />
+            
+            {/* 5. APPLICATION FLOW */}
+            <Route path="/register" element={<EducationForm />} />
+            <Route path="/education-details" element={<EducationForm />} />
+
+            {/* 6. 404 CATCH-ALL */}
+            <Route path="*" element={<Navigate to="/" replace />} />
           
+
             </Routes>
           </div>
         </Router>
